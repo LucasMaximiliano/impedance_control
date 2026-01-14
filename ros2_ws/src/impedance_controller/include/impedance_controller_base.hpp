ec0fe26d6ddad3575d6944922bc2a9aa806023b9
lucasmo.silva <lucasmo.silva@tum.de>
2026-01-14 16:46:47 +0100

    Finish draft of ImpedanceControllerBase class.

--- ros2_ws/src/impedance_controller/include/impedance_controller_base.hpp
+++ ros2_ws/src/impedance_controller/include/impedance_controller_base.hpp
@@ -6,7 +6,36 @@
 class ImpedanceControllerBase
 {
     public:
+        //! \name Constructor & Destructor
+        //! \param virtual_inertia_matrix Virtual inertia matrix (2x2)
+        //! \param virtual_damping_matrix Virtual damping matrix (2x2)
+        //! \param virtual_stiffness_matrix Virtual stiffness matrix (2x2)
+        //! \param force_feedback_gain Force feedback gain (scalar)
+        //! \{
+        ImpedanceControllerBase(
+            const Eigen::Matrix2d& virtual_inertia_matrix,
+            const Eigen::Matrix2d& virtual_damping_matrix,
+            const Eigen::Matrix2d& virtual_stiffness_matrix,
+            double force_feedback_gain);
+        ~ImpedanceControllerBase();
+        //! \}
 
+        //! \name Getters
+        //! \{
+        Eigen::Vector2d getTorqueCommanded() const;
+        //! \}
+
+        //! \name Control Law
+        //! \brief Computes the commanded torque based on the impedance control law
+        //! \details The control law is defined below. For more details, refer to the developer notes.
+        //!     \f[
+        //!     \tau_c=(1+K_F)J^T(q)[M^d(\ddot{x}^d-\ddot{x})+D^d(\dot{x}^d-\dot{x})+K^d(x^d-x)]+K_F\tau_e
+        //!     \f]
+        //! \return Commanded torque vector (2x1)
+        //! \{
+        Eigen::Vector2d controlLaw();
+        //! \}
+        
     private:
         //! \name Parameters
         //! \{
@@ -17,19 +46,20 @@ class ImpedanceControllerBase
         //! \}
 
         //! \name Input Variables
-        //! \{
-        Eigen::Vector2d position_measured_;     //!< Measured position vector (2x1)
-        Eigen::Vector2d velocity_measured_;     //!< Measured velocity vector (2x1)
-        Eigen::Vector2d torque_measured_;       //!< Measured torque vector (2x1)
-        Eigen::Vector2d position_desired_;      //!< Desired position vector (2x1)
-        Eigen::Vector2d velocity_desired_;      //!< Desired velocity vector (2x1)
-        Eigen::Vector2d acceleration_desired_;  //!< Desired acceleration vector (2x1)
+        //! \{        
+        Eigen::Vector2d cartesian_position_measured_;       //!< Measured position vector in Cartesian space (2x1)
+        Eigen::Vector2d cartesian_velocity_measured_;       //!< Measured velocity vector in Cartesian space (2x1)
+        Eigen::Vector2d cartesian_acceleration_computed_;   //!< Computed acceleration vector in Cartesian space (2x1)
+        Eigen::Vector2d cartesian_position_desired_;        //!< Desired position vector in Cartesian space (2x1)
+        Eigen::Vector2d cartesian_velocity_desired_;        //!< Desired velocity vector in Cartesian space (2x1)
+        Eigen::Vector2d cartesian_acceleration_desired_;    //!< Desired acceleration vector in Cartesian space (2x1)
+        Eigen::Vector2d joint_torque_measured_;             //!< Measured torque vector in joint space (2x1)
         //! \}
 
         //! \name Output Variables
         //! \{
-        Eigen::Vector2d torque_commanded_;    //!< Commanded torque vector (2x1)
+        Eigen::Vector2d joint_torque_commanded_;    //!< Commanded torque vector in joint space (2x1)
         //! \}
-}
+};
 
 #endif // IMPEDANCE_CONTROLLER_BASE_HPP
---
+++ ros2_ws/src/impedance_controller/src/impedance_controller_base.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include "impedance_controller_base.hpp"
+
+ImpedanceControllerBase::ImpedanceControllerBase(
+    const Eigen::Matrix2d& virtual_inertia_matrix,
+    const Eigen::Matrix2d& virtual_damping_matrix,
+    const Eigen::Matrix2d& virtual_stiffness_matrix,
+    double force_feedback_gain)
+    : virtual_inertia_matrix_(virtual_inertia_matrix),
+      virtual_damping_matrix_(virtual_damping_matrix),
+      virtual_stiffness_matrix_(virtual_stiffness_matrix),
+      force_feedback_gain_(force_feedback_gain),
+      joint_position_measured_(Eigen::Vector2d::Zero()),
+      joint_velocity_measured_(Eigen::Vector2d::Zero()),
+      joint_torque_measured_(Eigen::Vector2d::Zero()),
+      joint_position_desired_(Eigen::Vector2d::Zero()),
+      joint_velocity_desired_(Eigen::Vector2d::Zero()),
+      joint_acceleration_desired_(Eigen::Vector2d::Zero()),
+      cartesian_position_measured_(Eigen::Vector2d::Zero()),
+      cartesian_velocity_measured_(Eigen::Vector2d::Zero()),
+      cartesian_torque_measured_(Eigen::Vector2d::Zero()),
+      cartesian_position_desired_(Eigen::Vector2d::Zero()),
+      cartesian_velocity_desired_(Eigen::Vector2d::Zero()),
+      cartesian_acceleration_desired_(Eigen::Vector2d::Zero()),
+      torque_commanded_(Eigen::Vector2d::Zero())
+{
+    std::cout << "[ImpedanceControllerBase] Constructor called with..." << std::endl;
+    std::cout << "  Virtual Inertia Matrix:\n" << virtual_inertia_matrix_ << std::endl;
+    std::cout << "  Virtual Damping Matrix:\n" << virtual_damping_matrix_ << std::endl;
+    std::cout << "  Virtual Stiffness Matrix:\n" << virtual_stiffness_matrix_ << std::endl;
+    std::cout << "  Force Feedback Gain: " << force_feedback_gain_ << std::endl;
+    std::cout << "  Remaining member variables initialized to zero." << std::endl;
+}
+
+ImpedanceControllerBase::~ImpedanceControllerBase()
+{
+    std::cout << "[ImpedanceControllerBase] Destructor called." << std::endl;
+}
+
+Eigen::Vector2d ImpedanceControllerBase::getTorqueCommanded() const
+{
+    return torque_commanded_;
+}
+
+ImpedanceControllerBase::controlLaw()
+{
+    // Compute errors
+    Eigen::Vector2d position_error     = cartesian_position_desired_     - cartesian_position_measured_;
+    Eigen::Vector2d velocity_error     = cartesian_velocity_desired_     - cartesian_velocity_measured_;
+    Eigen::Vector2d acceleration_error = cartesian_acceleration_desired_ - cartesian_acceleration_computed_;
+
+    // Compute commanded torque using the impedance control law
+    torque_commanded_ = (1.0 + force_feedback_gain_) * 
+                        (virtual_inertia_matrix_ * acceleration_error +
+                         virtual_damping_matrix_ * velocity_error +
+                         virtual_stiffness_matrix_ * position_error) +
+                        force_feedback_gain_ * torque_measured_;
+
+    return torque_commanded_;
+}
+
+Eigen::Vector2d ImpedanceControllerBase::forward_kinematics(const Eigen::Vector2d& joint_positions)
+{
+    
+    return cartesian_positions;
+}
