a92502d1212f2daa55559e148e20f28c667fde9d
lucasmo.silva <lucasmo.silva@tum.de>
2026-01-21 17:33:53 +0100

    Add doxy comments to gravity compensation code.

--- ros2_ws/src/impedance_controller/include/gravity_compensation.hpp
+++ ros2_ws/src/impedance_controller/include/gravity_compensation.hpp
@@ -3,6 +3,18 @@
 
 #include <Eigen/Dense>
 
+//! \name computeGravityTorque
+//! \brief Computes the gravity compensation torque for a 2-DOF planar robotic finger
+//! \details This function calculates the gravity compensation torque required at each joint
+//! of a 2-DOF planar robotic finger with revolute joints. The calculation is based on the
+//! link masses, link lengths, distances to the center of mass, and current joint positions.
+//! It assumes a standard gravitational acceleration of 9.81 m/s² pointing downwards i.e. in
+//! the negative y-direction of the finger base coordinate system S_0.
+//! \param link_mass_vector Vector containing the masses of the links (2x1)
+//! \param link_length_vector Vector containing the lengths of the links (2x1)
+//! \param dist_to_link_com_vector Vector containing the distances from each joint to the center of mass of the respective link (2x1)
+//! \param joint_position Vector containing the current joint positions (2x1)
+//! \return Gravity compensation torque vector (2x1)
 Eigen::Vector2d computeGravityTorque(
     const Eigen::Vector2d& link_mass_vector,
     const Eigen::Vector2d& link_length_vector,
