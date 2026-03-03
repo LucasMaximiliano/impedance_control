82ca05a6ff4f4358c963a7e611bcf8e55897b63b
lucasmo.silva <lucasmo.silva@tum.de>
2026-03-03 17:19:21 +0100

    Start working on automated testing of ROS API before moving to hardware.

--- ros2_ws/src/impedance_controller_tests/src/matlab/CoordinateTransform.m
+++
@@ -1,8 +0,0 @@
-function [q1,q2] = CoordinateTransform(th1,th2)
-%COORDINATE_TRANSFORM Converts coordinates from original simulation code
-%   reference frame to our frame definition.
-%   - th1: Angle of the first joint  [rad]
-%   - th2: Angle of the second joint [rad]
-q1 = pi/2 + th1;
-q2 = th2;
-end
