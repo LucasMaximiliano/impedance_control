6cb8715681004bc7fa0e1aeeed72424a4f5a66da
lucasmo.silva <lucasmo.silva@tum.de>
2026-03-14 15:37:31 +0100

    Continue working on dummy planner python node.

--- ros2_ws/src/dummy_planner/dummy_planner/dummy_planner_node.py
+++ ros2_ws/src/dummy_planner/dummy_planner/dummy_planner_node.py
@@ -6,8 +6,25 @@ from std_msgs.msg import Float64MultiArray
 # Topic names and queue size
 QUEUE_SIZE = 10
 
-SUBSCRIBE_MEASURED_INITIAL_POSITION_TOPIC = '/senso_joint/position_rad'
-SUBSCRIBE_DESIRED_FINAL_POSITION_TOPIC    = 'blablabla' # TODO: Check with @Zara how to interface w/GUI
+SUBSCRIBE_MEASURED_INITIAL_POSITION_TOPIC = {
+    "name": 'senso_joint/position_rad',
+    "msg_type": Float64MultiArray
+}
+
+SUBSCRIBE_DESIRED_FINAL_POSITION_TOPIC    = {
+    "name": 'impedance_controller/set_final_position_rad',
+    "msg_type": Float64MultiArray
+}
+
+PUBLISH_DESIRED_POSITION_TOPIC = {
+    "name": 'impedance_controller/set_position_rad',
+    "msg_type": Float64MultiArray
+}
+
+PUBLISH_DESIRED_VELOCITY_TOPIC = {
+    "name": 'impedance_controller/set_velocity_rad_per_sec',
+    "msg_type": Float32MultiArray
+}
 
 class DummyPlannerNode(Node):
     def __init__(self):
@@ -15,15 +32,26 @@ class DummyPlannerNode(Node):
         self.get_logger().info('Dummy Planner Node has been started.')
         # Subscribers
         self.measured_initial_position_subscription_ = self.create_subscription(
-            Float64MultiArray,
-            SUBSCRIBE_MEASURED_INITIAL_POSITION_TOPIC,
+            SUBSCRIBE_MEASURED_INITIAL_POSITION_TOPIC["msg_type"],
+            SUBSCRIBE_MEASURED_INITIAL_POSITION_TOPIC["name"],
             self.measured_initial_position_callback,
             QUEUE_SIZE
         )
         self.desired_final_position_subscription_ = self.create_subscription(
-            Float64MultiArray,
-            SUBSCRIBE_DESIRED_FINAL_POSITION_TOPIC,
+            SUBSCRIBE_DESIRED_FINAL_POSITION_TOPIC["msg_type"],
+            SUBSCRIBE_DESIRED_FINAL_POSITION_TOPIC["name"],
             self.desired_final_position_callback,
+            QUEUE_SIZE
+        )
+        # Publishers
+        self.desired_position_publisher_ = self.create_publisher(
+            PUBLISH_DESIRED_POSITION_TOPIC["msg_type"],
+            PUBLISH_DESIRED_POSITION_TOPIC["name"],
+            QUEUE_SIZE
+        )
+        self.desired_velocity_publisher_ = self.create_publisher(
+            PUBLISH_DESIRED_VELOCITY_TOPIC["msg_type"],
+            PUBLISH_DESIRED_VELOCITY_TOPIC["name"],
             QUEUE_SIZE
         )
 
@@ -37,6 +65,11 @@ class DummyPlannerNode(Node):
 
 def main():
     print('Hi from dummy_planner.')
+    rclpy.init()
+    node = DummyPlannerNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
