9748e277de2f02ade86f69c4a38ddbdba13ffb39
lucasmo.silva <lucasmo.silva@tum.de>
2026-03-20 13:38:18 +0100

    Reproduce logic for all four fingers so the interface is consistent with the controller.

--- ros2_ws/src/dummy_planner/dummy_planner/dummy_planner_node.py
+++ ros2_ws/src/dummy_planner/dummy_planner/dummy_planner_node.py
@@ -59,8 +59,14 @@ class DummyPlannerNode(Node):
     ----------
     planner_enabled_ : bool
         a flag indicating whether the planner is enabled or not.
-    trajectory_ready_ : bool
-        a flag indicating whether the trajectory has been calculated and is ready for execution.
+    trajectory_ready_1_ : bool
+        a flag indicating whether the trajectory for finger 1 has been calculated and is ready for execution.
+    trajectory_ready_2_ : bool
+        a flag indicating whether the trajectory for finger 2 has been calculated and is ready for execution.
+    trajectory_ready_3_ : bool
+        a flag indicating whether the trajectory for finger 3 has been calculated and is ready for execution.
+    trajectory_ready_4_ : bool
+        a flag indicating whether the trajectory for finger 4 has been calculated and is ready for execution.
     trajectory_index_ : int
         an integer index for iterating through the trajectory during execution.
     timer_ : rclpy.timer.Timer
@@ -79,8 +85,14 @@ class DummyPlannerNode(Node):
         a list storing the measured initial position from the encoders.
     desired_final_position_ : numpy.ndarray
         a list storing the desired final position.
-    trajectory_ : numpy.ndarray
-        a list storing the calculated trajectory.
+    trajectory_1_ : numpy.ndarray
+        a list storing the calculated trajectory for finger 1.
+    trajectory_2_ : numpy.ndarray
+        a list storing the calculated trajectory for finger 2.
+    trajectory_3_ : numpy.ndarray
+        a list storing the calculated trajectory for finger 3.
+    trajectory_4_ : numpy.ndarray
+        a list storing the calculated trajectory for finger 4.
 
     Methods
     -------
@@ -104,7 +116,10 @@ class DummyPlannerNode(Node):
         self.get_logger().info('Dummy Planner Node has been started.')
         # Variables
         self.planner_enabled_ = False
-        self.trajectory_ready_ = False
+        self.trajectory_ready_1_ = False
+        self.trajectory_ready_2_ = False
+        self.trajectory_ready_3_ = False
+        self.trajectory_ready_4_ = False
         self.trajectory_index_ = 0
         # Timer
         self.timer_ = self.create_timer(CONTROL_LOOP_DURATION_MS / 1000.0, self.timer_callback)
@@ -142,24 +157,30 @@ class DummyPlannerNode(Node):
         """Callback function for receiving and storing the measured initial position from the encoders.
         
         Turns the received message from `SUBSCRIBE_MEASURED_INITIAL_POSITION_TOPIC` into a numpy array
-        and stores it in the class variable `measured_initial_position_`.
+        and stores it in the class variable `measured_initial_position_i_`, where `i` is the finger index
+        ranging from 1 to 4.
 
         Parameters
         ----------
         msg : std_msgs.msg.Float64MultiArray
             the message containing the measured initial position from the encoders.
         """
-        self.measured_initial_position_ = np.array(msg.data)
+        self.measured_initial_position_1_ = np.array(msg.data[0:2])
+        self.measured_initial_position_2_ = np.array(msg.data[2:4])
+        self.measured_initial_position_3_ = np.array(msg.data[4:6])
+        self.measured_initial_position_4_ = np.array(msg.data[6:8])
 
     def desired_final_position_callback(self, request, response):
         """Callback function for receiving the desired final position, calculating the trajectory, and
         storing it for execution.
 
         Turns the received desired final position from the `SET_DESIRED_FINAL_POSITION_SERVICE` request into
-        a numpy array and stores it in the class variable `desired_final_position_`. If the measured initial
-        position has already been received, it calculates the trajectory with a trapezoidal velocity profile
-        from the measured initial position to the desired final position, samples the trajectory at the
-        control loop frequency, and stores it in the class variable `trajectory_`.
+        a numpy array and stores it in the class variable `desired_final_position`. Note that all fingers
+        receive the same desired final position. However, this can easily be modified, upon needed. If the
+        measured initial position has already been received, it calculates the trajectory with a trapezoidal
+        velocity profile from the measured initial position to the desired final position, samples the
+        trajectory at the control loop frequency, and stores it in the class variable `trajectory_i_`, where
+        `i` is also the finger index.
 
         Parameters
         ----------
@@ -174,23 +195,34 @@ class DummyPlannerNode(Node):
             the response containing a success flag and a message indicating the result of the operation.
         """
         self.desired_final_position_ = np.array(request.data)
-        if hasattr(self, 'measured_initial_position_'):
+        if (hasattr(self, 'measured_initial_position_1_') and
+            hasattr(self, 'measured_initial_position_2_') and
+            hasattr(self, 'measured_initial_position_3_') and
+            hasattr(self, 'measured_initial_position_4_')):
             self.get_logger().info('Received desired final position: ' + str(self.desired_final_position_) +
                 ' rad. Calculating trajectory from measured initial position: ' + str(self.measured_initial_position_) +
                 ' rad.')
-            # Generate trajectory with trapezoidal velocity profile (sampled at the control loop frequency)
+            # Generate a trajectory with trapezoidal velocity profile  for each finger (sampled at the control loop frequency)
             trajectory_duration_sec = ( (MAX_JOINT_ACCELERATION_RAD_PER_SEC_SQ + MAX_JOINT_VELOCITY_RAD_PER_SEC**2) /
                 (MAX_JOINT_ACCELERATION_RAD_PER_SEC_SQ * MAX_JOINT_VELOCITY_RAD_PER_SEC) )
             time_array = np.arange(0, trajectory_duration_sec, CONTROL_LOOP_DURATION_MS / 1000.0)
-            self.trajectory_ = mtraj(trapezoidal, self.measured_initial_position_, self.desired_final_position_, time_array)
-            self.trajectory_ready_ = True
+            self.trajectory_1_ = mtraj(trapezoidal, self.measured_initial_position_1_, self.desired_final_position_, time_array)
+            self.trajectory_2_ = mtraj(trapezoidal, self.measured_initial_position_2_, self.desired_final_position_, time_array)
+            self.trajectory_3_ = mtraj(trapezoidal, self.measured_initial_position_3_, self.desired_final_position_, time_array)
+            self.trajectory_4_ = mtraj(trapezoidal, self.measured_initial_position_4_, self.desired_final_position_, time_array)
+            self.trajectory_ready_1_ = True
+            self.trajectory_ready_2_ = True
+            self.trajectory_ready_3_ = True
+            self.trajectory_ready_4_ = True
             # Write response
             response.message = 'Trajectory calculated successfully.'
             response.success = True
             self.get_logger().info(response.message)
         else:
             # Write response
-            response.message = 'Warning: Measured initial position not received.'
+            response.message = 'Warning: Measured initial position from finger(-s)' + \
+            str([i for i in range(1, 5) if not hasattr(self, f'measured_initial_position_{i}_')]) + \
+            ' not received.'
             response.success = False
             self.get_logger().warn(response.message)
         return response
@@ -239,18 +271,25 @@ class DummyPlannerNode(Node):
         completed, disables the planner, resets the trajectory ready flag, and resets
         the trajectory index for the next execution.
         """
-        if self.planner_enabled_ and self.trajectory_ready_:
+        if (self.planner_enabled_ and
+            self.trajectory_ready_1_ and
+            self.trajectory_ready_2_ and
+            self.trajectory_ready_3_ and
+            self.trajectory_ready_4_):
             desired_position_msg = Float64MultiArray()
             desired_velocity_msg = Float32MultiArray()
+            desired_position_msg.data = []
+            desired_velocity_msg.data = []
             # Format ROS messages
-            desired_position_msg.data = self.trajectory_.q[self.trajectory_index_, :].tolist()
-            desired_velocity_msg.data = self.trajectory_.qd[self.trajectory_index_, :].tolist()
+            for i in range(1, 5):
+                desired_position_msg.data.extend(getattr(self, f'trajectory_{i}_').q[self.trajectory_index_, :].tolist())
+                desired_velocity_msg.data.extend(getattr(self, f'trajectory_{i}_').qd[self.trajectory_index_, :].tolist())
             # Publish ROS messages and increment trajectory index for next run
             self.desired_position_publisher_.publish(desired_position_msg)
             self.desired_velocity_publisher_.publish(desired_velocity_msg)
             self.trajectory_index_ += 1
             # Check if end of trajectory is reached and reset variables if so
-            if self.trajectory_index_ >= self.trajectory_.__len__():
+            if self.trajectory_index_ >= self.trajectory_1_.__len__():
                 self.get_logger().info('Trajectory execution completed.')
                 self.planner_enabled_ = False
                 self.trajectory_ready_ = False
